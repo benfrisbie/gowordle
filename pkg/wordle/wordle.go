2815c4d846443db0bd46bdf11b7c70c186ba2b1e
Ben Frisbie <bbfrisbi@gmail.com>
2022-08-06 16:29:53 -0400

    fix #4

--- pkg/wordle/wordle.go
+++ pkg/wordle/wordle.go
@@ -6,7 +6,7 @@ import (
 
 // Wordle represents a wordle game and the necessary state
 type Wordle struct {
-	Word       string
+	Solution   string
 	Guesses    int
 	MaxGuesses int
 	Win        bool
@@ -14,17 +14,15 @@ type Wordle struct {
 }
 
 // NewWordle properly initializes a new Wordle game
-func NewWordle(word string, maxGuesses int) *Wordle {
+func NewWordle(solution string, maxGuesses int) *Wordle {
 	w := Wordle{}
-	w.Word = word
+	w.Solution = solution
+	w.Solution = "exits"
 	w.Guesses = 0
 	w.MaxGuesses = maxGuesses
 
 	// initialize rune hints
-	w.RuneHints = make(map[rune]Hint)
-	for _, r := range GetAlphabet() {
-		w.RuneHints[r] = HintUnused
-	}
+	w.RuneHints = NewHintMap()
 
 	return &w
 }
@@ -33,22 +31,34 @@ func NewWordle(word string, maxGuesses int) *Wordle {
 func (w *Wordle) Guess(guess string) string {
 	w.Guesses++
 	hint := ""
-	for i, r := range guess {
-		if guess[i] == w.Word[i] {
-			w.RuneHints[r] = HintCorrect
-			hint += HintCorrect.String(r)
-		} else if strings.Contains(w.Word, string(guess[i])) {
-			w.RuneHints[r] = HintIncorrectPosition
-			hint += HintIncorrectPosition.String(r)
+	for i, guessRune := range guess {
+		solutionRune := rune(w.Solution[i])
+		if guessRune == solutionRune {
+			w.UpdateHintMap(guessRune, HintCorrect)
+			hint += HintCorrect.String(guessRune)
+		} else if count := strings.Count(w.Solution, string(guessRune)); count > 0 && count != w.getCorrectCountForLetter(guess, guessRune) {
+			w.UpdateHintMap(guessRune, HintIncorrectPosition)
+			hint += HintIncorrectPosition.String(guessRune)
 		} else {
-			w.RuneHints[r] = HintIncorrect
-			hint += HintIncorrect.String(r)
+			w.UpdateHintMap(guessRune, HintIncorrect)
+			hint += HintIncorrect.String(guessRune)
 		}
 	}
-	if guess == w.Word {
+	if guess == w.Solution {
 		w.Win = true
 	}
 	return hint
+}
+
+// getCorrectCountForLetter returns the number of times a letter is in the correct position in the solution
+func (w *Wordle) getCorrectCountForLetter(guess string, r rune) int {
+	count := 0
+	for i, solutionRune := range w.Solution {
+		if solutionRune == r && rune(guess[i]) == r {
+			count++
+		}
+	}
+	return count
 }
 
 // GetAlphabetHints returns the current games hints for all letters in the alphabet
